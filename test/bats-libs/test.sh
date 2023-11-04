b52ecad3c490a5ecbd316a585447b3b7adf2cda4
brokenpip3 <brokenpip3@gmail.com>
2023-11-04 23:17:15 +0100

    typo

--- test/bats-libs/test.sh
+++ test/bats-libs/test.sh
@@ -3,7 +3,7 @@ source dev-container-features-test-lib
 
 check "Bats support directory is present" test -d "/usr/lib/bats/bats-support"
 check "Bats assert directory is present" test -d "/usr/lib/bats/bats-assert"
-check "Bats detik directory is present" test -d "/usr/lib/bats/bats-detiks"
+check "Bats detik directory is present" test -d "/usr/lib/bats/bats-detik"
 check "Bats file directory is present" test -d "/usr/lib/bats/bats-file"
 
 reportResults
